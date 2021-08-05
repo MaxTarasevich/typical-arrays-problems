3cfd77ee7874a2aa1a9a120dec1556d78cbc1f77
Max <maxtarasevich@gmail.com>
2021-08-05 13:11:05 +0300

    feat: add min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!Array.isArray(array)){
+    return 0
+  }
+  if(array.length == 0 ){
+    return 0
+  }
+  array.sort((a,b)=>a-b)
+  return array[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!Array.isArray(array)){
+    return 0
+  }
+  if(array.length == 0 ){
+    return 0
+  }
+  array.sort((a,b)=>a-b)
+  return array[array.length-1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!Array.isArray(array)){
+    return 0
+  }
+  if(array.length == 0 ){
+    return 0
+  }
+  let rez = (array.reduce((acc,val)=>acc+val)) / array.length
+  return rez;
 }
